705b0635d69552cb85b9727d4e4ae1a5c2b04d5b
Disha Gupta <disha.guptaa2005@gmail.com>
2026-05-16 21:46:39 +0530

    Change ServerUrl to production endpoint

--- HIREIQ/client/src/App.jsx
+++ HIREIQ/client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = "https://mern-3z7u.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
